68b514dca93484076025561acfd9d120ee17e2e4
akshay vinchurkar <akshayv@vcsbim.com>
2017-11-03 08:49:53 +0530

    navbar component created reference helloworld component

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <navbar/>
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import navbar from './components/navbar'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    navbar
+  }
+}
+</script>
+
+<style>
+body{
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="navbar">
+		<ul>
+			<li v-for="navbarlink in links">
+				<a v-bind:href="''+navbarlink.linkaddress+''">{{ navbarlink.linkname }}</a>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+  data () {
+    return {
+      links: [{linkname: 'Home', linkaddress: 'https://www.google.com'}, {linkname: 'About', linkaddress: 'https://www.google.com'}, {linkname: 'Blog', linkaddress: 'https://www.google.com'}, {linkname: 'Author', linkaddress: 'https://www.google.com'}, {linkname: 'Work', linkaddress: 'https://www.google.com'}]
+    }
+  }
+}
+</script>
+
+<style scoped>
+	.navbar {
+		width: 100%;
+		height: 100px;
+		background-color: #333;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.navbar ul {
+		 list-style: none;
+	}
+
+	.navbar ul li {
+		 display: inline-block;
+		 list-style: none;
+		 margin-right: 40px;
+	}
+
+	.navbar ul li a{
+		 text-decoration: none;
+		 color: #fff;
+		 font-size: 14px;
+		 text-transform: uppercase;
+		 font-weight: 400;
+	}
+</style>
